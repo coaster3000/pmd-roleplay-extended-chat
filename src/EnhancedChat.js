2082c89ce2016af0f443c446a5b2a1e6878e4128
Christopher Krier <ckrier.3000@gmail.com>
2016-10-30 22:41:26 -0400

    Add initial constructor for EnhancedChatbox object.

--- src/EnhancedChat.js
+++ src/EnhancedChat.js
@@ -1,1 +1,13 @@
 	//Enhanced Chat
+
+	class EnhancedChatbox {
+		
+		constructor (chatbox) {
+			this.cb = chatbox;
+			
+			this.scheduler = new Schedular();
+			this.globalDelay = 50;
+		}
+		
+		
+	}
